c872a39725b058655221ac4b65e4a57085d376fa
Jacob Trevor <J.Trevor.1@research.gla.ac.uk>
2026-07-21 17:17:47 +0100

    old stab at modular programs

--- Semantics/modular.lean
+++ Semantics/modular.lean
@@ -1,52 +1,38 @@
-/-
-  If we just blindly apply Swierstra's DTalC, we end
-  up with a non-positive definition, which lean does not permit.
-  So we need to find a way to deal with that.
-  HOAS is another very powerful technique which is unfortunately non-positive
-  PHOAS solves this by removing the need for a negative occurence
-  and instead replacing it with a parameter of the type
-  which we can instantiate with the type itself as and when.
 
-  The positivity is broken here
--/
-inductive Expr {X : Nat -> Type} (F : _ -> _)  where
-  | mk : F X -> Expr F
+structure Program (Command : Type) where
+  cmds : List Command
 
-inductive myProd (A B : (Nat -> Type) -> Type) : (Nat -> Type) -> Type where
-  | left : A X -> myProd A B X
-  | right : B X -> myProd A B X
-
-
-infix:50 ":+:" => myProd
 
 inductive Gate (n : Nat) where
 inductive PartialPerm (n m : Nat) where
 
--- modular definition:
-inductive pDone (X : Nat -> Type) : Type where
-  | done : pDone X
+inductive App where
+  | app : (h_size : n <= i) ->  Gate n -> PartialPerm n i -> App
 
-inductive App (X : Nat -> Type) : Type where
-  | app : (h_size : n <= i) ->  Gate n -> PartialPerm n i -> X i -> App X
+inductive Meas where
+  | meas : Fin i -> Meas
 
-inductive Meas (X : Nat -> Type) : Type where
-  | meas : Fin i -> X i -> Meas X
+inductive Classical {CInstr} where
+  | instr : CInstr -> Classical
 
-inductive Classical {CInstr} (X : Nat -> Type) : Type where
-  | instr : CInstr -> X i -> Classical X
+inductive Cond {CAddr} Body where
+  | if : CAddr -> Body -> Cond Body
 
-inductive Cond {CAddr} (X : Nat -> Type) : Type where
-  | if : CAddr -> X i -> X i -> Cond X
+inductive CoCond {CAddr} where
+  | mres : Fin i -> CAddr -> CoCond
 
-inductive CoCond {CAddr} (X : Nat -> Type) : Type where
-  | mres : Fin i -> CAddr -> X i -> CoCond X
+inductive Alloc i where
+  | alloc :          Alloc i
+  | Free  : Fin i -> Alloc i
+
+/-
+  Error:
+-/
+#guard_msgs(substring:=true) in
+structure ExprBroken (f : Type -> Type) where
+  data : f (ExprBroken f)
 
 
-def Unitary {X} := @Expr X <| pDone :+: App
-
-
---- Attempt 3
-
-
-structure Program Command where
-  cmds : List Command
+-- instance {C X : Type} [DenotSem C X] [Monoid X] [Zero X]: DenotSem (Program C) X where
+--   sem c := c.cmds
+--   -- sem .mk x::xs = D⟦x⟧ + D⟦xs⟧ --recurse
